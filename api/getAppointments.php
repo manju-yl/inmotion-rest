d2e92cdd57a881da4dd126e176870f1240711606
= <manjunath.l@impelsys.com>
2021-04-12 15:59:52 +0530

    get appointment details api

---
+++ api/getAppointments.php
@@ -0,0 +1,82 @@
+<?php
+
+include_once './config/database.php';
+include_once './model/appointment.php';
+require "../vendor/autoload.php";
+require "./common/headers.php";
+
+use \Firebase\JWT\JWT;
+
+$secret_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
+$databaseService = new DatabaseService();
+$conn = $databaseService->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+$authHeader = filter_input(INPUT_SERVER, 'HTTP_AUTHORIZATION', FILTER_SANITIZE_STRING);
+
+$arr = explode(" ", $authHeader);
+
+/* echo json_encode(array(
+  "message" => "sd" .$arr[1]
+  )); */
+
+$jwt = $arr[1];
+
+if ($jwt) {
+
+    try {
+
+        $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+
+        $appointment = new Appointment($conn);
+        $stmt = $appointment->getAppointments($data);
+        $num = $stmt->rowCount();
+
+        if ($num > 0) {
+            $user_arr = array();
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+                $user_item = array(
+                    "eventId" => $event_id,
+                    "eventName" => $event_name,
+                    "companyId" => $co_id,
+                    "companyName" => $company_name,
+                    "time" => $time,
+                    "day"  => $day,
+                    "companyContactFirstName" => $company_contact_first_name,
+                    "companyContactLastName" => $company_contact_last_name,
+                    "companyEmail" => $company_email
+                );
+                
+                array_push($user_arr, $user_item);
+            }
+
+            // set response code - 200 OK
+            http_response_code(200);
+
+            // show user data in json format
+            echo json_encode($user_arr);
+        } else {
+
+            // set response code - 404 Not found
+            http_response_code(404);
+
+            // no appointments found
+            echo json_encode(
+                    array("message" => "No Appointments found.")
+            );
+        }
+    } catch (Exception $e) {
+
+        http_response_code(401);
+
+        echo json_encode(array(
+            "message" => "Access denied.",
+            "error" => $e->getMessage()
+        ));
+    }
+}
+
+
+
---
+++ api/model/appointment.php
@@ -0,0 +1,71 @@
+<?php
+
+class Appointment {
+
+    private $conn;
+    private $table_name = "appointment";
+    // properties
+    public $co_id;
+    public $company_name;
+    public $event_id;
+    public $event_name;
+    public $day;
+    public $time;
+    public $company_contact_first_name;
+    public $company_contact_last_name;
+    public $company_email;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    //get user details
+    function getAppointments($data) {
+        $company_email = "";
+        $event_id = filter_var($data->event_id, FILTER_SANITIZE_NUMBER_INT);
+        $company_id = filter_var($data->company_id, FILTER_SANITIZE_NUMBER_INT);
+        $company_email = filter_var($data->company_email, FILTER_SANITIZE_EMAIL);
+        $addCondition = "";
+        if ($company_id != "") {
+            $addCondition = "and a.company_id = ?";
+        }
+        if ($company_email != "") {
+            $addCondition .= " and c.company_email = ?";
+        }
+        // select all query
+        $query = "SELECT 
+                        c.co_id,
+                        c.company_name,
+                        e.event_id,
+                        e.event_name,
+                        a.day,
+                        a.time,
+                        c.company_contact_first_name,
+                        c.company_contact_last_name,
+                        c.company_email
+                    FROM
+                        " . $this->table_name . " a
+                            LEFT JOIN
+                        company c ON a.company_id = c.co_id
+                            LEFT JOIN
+                        event e ON e.event_id = a.event_id
+                    WHERE
+                        a.event_id = ? ";
+        $query .= $addCondition;
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, htmlspecialchars(strip_tags($event_id)));
+        if ($company_id != "") {
+            $stmt->bindParam(2, htmlspecialchars(strip_tags($company_id)));
+        }
+        if ($company_email != "") {
+            $stmt->bindParam(3, strip_tags($company_email));
+        }
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+}
